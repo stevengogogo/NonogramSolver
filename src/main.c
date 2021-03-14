60aa799c414d41609bd0da49551e1c5c5c322753
stevengogogo <stevengogogo4321@gmail.com>
2021-03-14 17:50:59 +0800

    Fix nonogram creation

--- src/main.c
+++ src/main.c
@@ -11,39 +11,23 @@
 /* * @brief Interface */
 int main()
 {
-     struct array{
-        int arr[4];
-        int len;
-        int n_seg;
-    };
+    nogram nog;
+    //nog = create_nogram_scantf();
+    nog = create_nogram_fscantf("test/data/input_1.txt");
 
-    struct array A[] = {
-        {{0,0,0,0},4,0},
-        {{1,0,0,0},4,1},
-        {{0,1,0,0},4,1},
-        {{1,1,0,0},4,1},
-        {{0,0,1,0},4,1},
-        {{1,0,1,0},4,2},
-        {{0,1,1,0},4,1},
-        {{1,1,1,0},4,1},
-        {{0,0,0,1},4,1},
-        {{1,0,0,1},4,2},
-        {{0,1,0,1},4,2},
-        {{1,1,0,1},4,2},
-        {{0,0,1,1},4,1},
-        {{1,0,1,1},4,2},
-        {{0,1,1,1},4,1},
-        {{1,1,1,1},4,1}
-    };
-    int A_len = 16;
-    hint h;
+    int a0[4] = {1,0,1,0};
+    int a1[4] = {0,1,0,1};
+    int a2[4] = {1,0,1,0};
+    int a3[4] = {1,1,1,1};
 
-    for(int i=0;i<A_len;i++){
-        h = get_segments(A[i].arr, A[i].len);
-        printf("\nGet %d; true %d",h.nPoint, A[i].n_seg);
-        assert(A[i].n_seg == h.nPoint);
-        //TEST_MSG("True seg: %d. But got %d", A[i].n_seg, h.nPoint);
-    }
+    for(int i=0;i<4;i++)
+        nog.map[0][i] = a0[i];
+    for(int i=0;i<4;i++)
+        nog.map[1][i] = a1[i];
+    for(int i=0;i<4;i++)
+        nog.map[2][i] = a2[i];
+    for(int i=0;i<4;i++)
+        nog.map[3][i] = a3[i];
 
-    return 0;
+    assert(is_nogram_valid(nog) == 1);
 }
