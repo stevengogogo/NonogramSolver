45551d7c6f712b48f9a753285c3d1af12a9249af
stevengogogo <stevengogogo4321@gmail.com>
2021-03-15 15:03:36 +0800

    align

--- test/test_Nonogram.h
+++ test/test_Nonogram.h
@@ -163,7 +163,7 @@ void test_greedy_algorithm(void){
     //TODO
     nogram nog;
     
-    nog =  create_nogram_fscantf("test/data/input_1.txt");
+    nog = create_nogram_fscantf("test/data/input_1.txt");
     nog = solve_nonogram_greedy(nog);
 
     TEST_ASSERT(is_nogram_valid(nog) == 1);
