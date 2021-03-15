66d1a5dd69c0b791d866d259540ed2ea45cb4716
stevengogogo <stevengogogo@users.noreply.github.com>
2021-03-15 08:05:49 +0000

    deploy: 0c1375c427eb38c937e614eae5edeb543fe754e1

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['get_5fsegments_8',['get_segments',['../NonogramSolver_8h.html#a27fe7bff76f35998088ce5017fb46552',1,'get_segments(int line[], int len_line):&#160;NonogramSolver.c'],['../NonogramSolver_8c.html#a27fe7bff76f35998088ce5017fb46552',1,'get_segments(int line[], int len_line):&#160;NonogramSolver.c']]]
+  ['get_5fsegments_8',['get_segments',['../NonogramSolver_8c.html#a27fe7bff76f35998088ce5017fb46552',1,'get_segments(int line[], int len_line):&#160;NonogramSolver.c'],['../NonogramSolver_8h.html#a27fe7bff76f35998088ce5017fb46552',1,'get_segments(int line[], int len_line):&#160;NonogramSolver.c']]]
 ];
