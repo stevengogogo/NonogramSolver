8e217a3a1234e72b5020aa88e12718a61d62cbdd
stevengogogo <stevengogogo@users.noreply.github.com>
2021-03-16 02:55:11 +0000

    deploy: ef0421b8e22e2ab9649551742825ff54b6e98439

--- search/all_c.js
+++ search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['segment_5fnumber_32',['segment_number',['../NonogramSolver_8c.html#afd5e8266d0f0761447eb9d04bdd3546f',1,'segment_number(int line[], int len_line, int key):&#160;NonogramSolver.c'],['../NonogramSolver_8h.html#afd5e8266d0f0761447eb9d04bdd3546f',1,'segment_number(int line[], int len_line, int key):&#160;NonogramSolver.c']]],
+  ['segment_5fnumber_32',['segment_number',['../NonogramSolver_8c.html#a147397e90e9df6c1d1aec9cc6ad0b69b',1,'segment_number(dymarr *line, int len_line, int key):&#160;NonogramSolver.c'],['../NonogramSolver_8h.html#a147397e90e9df6c1d1aec9cc6ad0b69b',1,'segment_number(dymarr *line, int len_line, int key):&#160;NonogramSolver.c']]],
   ['set_5fnonogram_5fanswer_33',['set_nonogram_answer',['../NonogramSolver_8c.html#ad6fa23ab472ed25c4233239304afb95a',1,'set_nonogram_answer(nogram *nptr, char *output_fn):&#160;NonogramSolver.c'],['../NonogramSolver_8h.html#a80eb12413419b8d1777c71fef0596f45',1,'set_nonogram_answer(nogram *, char *output_fn):&#160;NonogramSolver.c']]],
   ['size2d_34',['size2D',['../structsize2D.html',1,'']]],
   ['solve_5fnonogram_35',['solve_nonogram',['../NonogramSolver_8c.html#a96a161e430d37e1430e21b4f9ff11fa4',1,'solve_nonogram(nogram *nog):&#160;NonogramSolver.c'],['../NonogramSolver_8h.html#a96a161e430d37e1430e21b4f9ff11fa4',1,'solve_nonogram(nogram *nog):&#160;NonogramSolver.c']]],
